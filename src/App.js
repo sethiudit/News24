fc7b16e4cd968dc1b9b491087d6ec87dd9d47ebc
[Rajat Jakhar] <[rajatjakhar03@gmail.com]>
2022-05-21 21:13:06 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>hello news app</div>
+    )
+  }
+}
